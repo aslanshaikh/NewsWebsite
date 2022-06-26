944993ed5ef114532cdfc3cfe6913830c9400008
aslanshaikh <90238727+aslanshaikh@users.noreply.github.com>
2022-06-26 11:53:26 +0530

    Update Nvabar.js

--- src/Components/Nvabar.js
+++ src/Components/Nvabar.js
@@ -10,7 +10,7 @@ export class Nvabar extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="NewsWebsite/">NewsHub</a>
+                        <a className="navbar-brand" href="/NewsWebsite/">NewsHub</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
