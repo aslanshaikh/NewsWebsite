f0b73bdb1ef7ad257d97e72b06723461cfe1ac2f
aslanshaikh <90238727+aslanshaikh@users.noreply.github.com>
2022-06-26 11:45:05 +0530

    Update Nvabar.js

--- src/Components/Nvabar.js
+++ src/Components/Nvabar.js
@@ -17,7 +17,7 @@ export class Nvabar extends Component {
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <Link className="nav-link active" aria-current="page" href="aslanshaikh.github.io/NewsWebsite/">Home</Link>
+                                    <a className="nav-link active" aria-current="page" href="/NewsWebsite/">Home</a>
                                 </li>
                                
                                  <li className="nav-item">
