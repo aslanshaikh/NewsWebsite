2433b557ae088d8a4f2aea59aa296ec51daeb8cf
aslanshaikh <90238727+aslanshaikh@users.noreply.github.com>
2022-06-26 11:38:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default class App extends Component {
         <Routes>
           <Route exact path="/" element={<News key="technology"  country="in" pageSize={this.pageSize} category="technology"/> } />
          
-          <Route exact path="/Business" element={<News key="business"  country="in" pageSize={this.pageSize} category="business"/> } />
+          <Route exact path="NewsWebsite/Business" element={<News key="business"  country="in" pageSize={this.pageSize} category="business"/> } />
           <Route exact path="/General" element={<News key="general"  country="in" pageSize={this.pageSize} category="general"/> } />
           <Route exact path="/Entertainment" element={<News key="entertainment"  country="in" pageSize={this.pageSize} category="entertainment"/> } />
           <Route exact path="/Health" element={<News key="health"  country="in" pageSize={this.pageSize} category="health"/> } />
